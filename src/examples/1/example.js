544f1477ad4557ddcf37b15f96df08fcb0a43bf6
Anton Balashov <balashov.anton.1983@gmail.com>
2019-12-20 16:08:55 +0200

    Pass example 1

--- src/examples/1/example.js
+++ src/examples/1/example.js
@@ -6,6 +6,10 @@
  * Redux Saga существует специальный эффект.
  * Примеры эффектов в этом файле — take и put.
  * put — самый простой эффект, он работает как store.dispatch.
+ * 
+ * Однако предпочтительней использовать эффекты всегда.
+ * Это улучшает тестируемость саги-генератора,
+ * явность происходящего в ней, а также консистентность написаемого кода.
  */
 
 // Core
@@ -16,15 +20,20 @@ import { types } from '../../bus/swapi/types';
 import { swapiActions } from '../../bus/swapi/actions';
 import { api } from '../../Api';
 
+// watcher и worker одновременно
 export function* runExample() {
+    console.log('-> message 1');
     while (true) {
+        // watcher, blocks current saga and then watches for action
+        // call & apply are also effects that block
         const action = yield take(types.FETCH_PLANETS_ASYNC);
+        console.log('-> message 2');
 
-        yield put(swapiActions.setIsFetching(true));
+        //yield put(swapiActions.setIsFetching(true));
         const response = yield api.fetchPlanets(action.payload);
         const data = yield response.json();
 
         yield put(swapiActions.fillPlanets(data.results));
-        yield put(swapiActions.setIsFetching(false));
+        //yield put(swapiActions.setIsFetching(false));
     }
 }
